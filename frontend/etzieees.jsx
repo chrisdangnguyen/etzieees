408960e5fa5d5ae589a0f2493448935ce1ca2d6b
chrisdangnguyen <cdnguyen24@gmail.com>
2020-01-07 16:10:15 -0800

    project skeleton with partial auth

---
+++ frontend/etzieees.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//Components
+import Root from './components/root';
+import configureStore from './store/store';
+import * as APIUtil from './util/session_api_util';
+
+document.addEventListener('DOMContentLoaded', () => {
+    let store;
+    if (window.currentUser) {
+        const preloadedState = {
+            session: { id: window.currentUser.id },
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser }
+            }
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
+    const root = document.getElementById('root');
+    window.signup = APIUtil.signup;
+    window.login = APIUtil.login;
+    window.logout = APIUtil.logout;
+    ReactDOM.render(<Root store={store} />, root);
+});
