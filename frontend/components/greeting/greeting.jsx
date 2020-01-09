80dfe7b49d549fa6f1ad6818d1da45078c376796
chrisdangnguyen <cdnguyen24@gmail.com>
2020-01-09 13:26:45 -0800

    edit css for modal

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 
 const Greeting = ({ currentUser, logout, openModal }) => {
     const sessionLinks = () => (
+        
         <div className="login-signup">
             <button className="signin-m" onClick={() => (openModal('login'))}>
                 Sign in
@@ -14,12 +15,18 @@ const Greeting = ({ currentUser, logout, openModal }) => {
                 Register
             </button>
         </div>
+
     );
     const personalGreeting = () => (
-        <hgroup className="header-group">
-            <h2 className="header-name">Welcome back, {currentUser.name}!</h2>
-            <button className="header-button" onClick={logout}>Sign Out</button>
-        </hgroup>
+        <div>
+            <div className="header-group">
+                <h2 className="header-name">Welcome back, {currentUser.name}!</h2>
+            </div>
+
+            <div className="dropdown-head">
+                <a href="#" onClick={logout} className="logout-btn">Sign out</a>
+            </div>
+        </div>
     );
 
     return currentUser ? personalGreeting() : sessionLinks();
--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -21,7 +21,8 @@ class SessionForm extends React.Component {
     handleSubmit(e) {
         e.preventDefault();
         const user = Object.assign({}, this.state);
-        this.props.processForm(user);
+        this.props.processForm(user)
+            .then(this.props.closeModal);
     }
 
     handleDemo(e) {
@@ -31,7 +32,8 @@ class SessionForm extends React.Component {
             name: "Demo",
             password: 'demo123'
         });
-        this.props.processForm(demo);
+        this.props.processForm(demo)
+            .then(this.props.closeModal);
     }
 
     renderErrors() {
@@ -47,61 +49,79 @@ class SessionForm extends React.Component {
     }
 
     render() {
-            const nameInput = this.props.formType === 'Register' 
-                ? (
-                <div className="login-form">
-                    <br />
-                    <label>First Name:
-                    <input type="text"
-                            value={this.state.name}
-                            onChange={this.update('name')}
-                            className="login-input"
-                        />
-                    </label>
-                    <br />
-                </div>
-            ) : ('');
 
-            const displayButton = this.props.formType === 'Sign in'
+        const header = this.props.formType === 'Register'
             ? (
-                <div>
-                    <button className="formtypebutton">{this.props.formType}</button>
-                    <button onClick={this.handleDemo} className="demobutton" id="demo-login">Demo Sign in</button>
+                <div className="create-account-div">
+                    <h2 className="create-header">Create your account</h2>
+                    <h3 className="regis-easy">Registration is easy</h3>
                 </div>
-            ) : (
-                <div>
-                    <button className="formtypebutton">{this.props.formType}</button>  {/*  register button */}
+            ) : ( 
+                <div className="sign-in-div">
+                    <h2 className="signin-header">Sign in to continue</h2>
                 </div>
             );
 
+        const nameInput = this.props.formType === 'Register' 
+            ? (
+            <div className="login-form-fname">
+                <br/>
+                <label>First Name
+                <br/>
+                <input type="text" value={this.state.name}
+                    onChange={this.update('name')} className="login-input-fname"/>
+                </label>
+            </div>
+            ) : ('');
+
+        const displayButton = this.props.formType === 'Sign in'
+            ? (
+            <div>
+                <button onClick={this.handleDemo} className="demobutton" 
+                    id="demo-login">Demo Sign in</button>
+            </div>
+            
+        ) : ('');
+
+
         return (
             <div className="login-form-container">
+                    {header}
                 <form onSubmit={this.handleSubmit} className="login-form-box">
-          <br />
                     {this.renderErrors()}
                     <div className="login-form">
-                        <br />
-                        {nameInput}
-                        <label>Email:
+                        <br/>
+                        <label>Email address
+                        <br/>
                          <input type="text"
                                 value={this.state.email}
                                 onChange={this.update('email')}
-                                className="login-input"
-                            />
+                                className="login-input"/>
                         </label>
+                        <br/>
+                        {nameInput}
                         <br />
-                        <label>Password:
+                        <label>Password
+                            <br/>
                         <input type="password"
                                 value={this.state.password}
                                 onChange={this.update('password')}
-                                className="login-input"
-                            />
+                                className="login-input"/>
                         </label>
                         <br />
+                        <br/>
+                        <button className="formtypebutton">
+                            {this.props.formType}
+                        </button> 
+                        <br/>
+                        <br/>
                         {displayButton}
                         {/* <input className="session-submit" type="submit" value={this.props.formType} /> */}
                         <br/> 
                     </div>
+                    {/* <button className="register-m" onClick={() => (openModal('signup'))}>
+                        Register
+                    </button> */}
                 </form>
             </div>
         );
