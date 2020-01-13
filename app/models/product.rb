d42d4e1d6925c76ce3839def7d2ea8b803c9101d
chrisdangnguyen <cdnguyen24@gmail.com>
2020-01-12 17:27:29 -0800

    update products model, controller, adn table

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,14 @@
 class Product < ApplicationRecord
-    validates :product_name, :price, :detail, :quantity, :catergory_id,
+    validates :product_name, :price, :detail, :quantity, :catergory,
         :user_id, prescence: true 
     validates :price, numericality: { greater_than: 0 }
     validates :quantity, numericality: { greater_than: 0 }
+    validates :category, inclusion: { in: %w(jewelry & accessories, clothings & shoes, 
+        home & living, toys & entertainment, art & collectibles, craft supplies, vintage),
+        message: "Please enter a valid category"}
 
     belongs_to :user
-    belongs_to :category
+
+    has_many_attached :images
 
 end
