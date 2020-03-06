2c5fe8fd06af7277c9d0627d532ed0a37a598fc0
chrisdangnguyen <cdnguyen24@gmail.com>
2020-03-06 11:33:37 -0800

    checkout css

--- frontend/components/cart_item/checkout_message.jsx
+++ frontend/components/cart_item/checkout_message.jsx
@@ -10,17 +10,18 @@ class CheckoutMessage extends React.Component {
   render() {
     return (
       <div className="checkout-container">
-        <div className="checkout">
+       
           <div className="checkout-message">
-            <h2>Thank you for your purchase on</h2>
+            <h2 className="message">Thank you for your purchase on</h2>
             <h2 className="checkout-title">Etzieees</h2>
+
+            <p>Come shop again soon!</p>
+            <Link to="/" onClick={this.props.closeModal} className="checkout-link">
+              Back to home
+            </Link>
           </div>
 
-          <p>Come shop again soon!</p>
-          <Link to="/" onClick={this.props.closeModal} className="checkout-modal">
-            Return back to home
-          </Link>
-        </div>
+        
       </div>
     )
 
